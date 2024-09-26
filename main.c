198a74e343aaefb8f0f048336ecaaa8a264949b6
mdazevedo96 <42869269+mdazevedo96@users.noreply.github.com>
2024-09-25 21:17:56 -0300

    Refatoração código main.c
    Função de alocação refatorada
    Criação da função imprime

--- main.c
+++ main.c
@@ -1,48 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void alocaMatriz(char*** matriz, int m, int n) {
-    *matriz = (char**) malloc(m * sizeof(char*));
-    if (*matriz == NULL) {
-        printf("Erro na alocacao");
+
+char ** inicia_matriz(int m, int n){
+    char ** matriz = NULL;
+    matriz = (char **) malloc (m * sizeof(char*));
+    if (matriz == NULL){
+        printf("Erro de alocacao");
         exit(1);
-    } else {
-        for (int i = 0; i < m; i++) {
-            (*matriz)[i] = (char*) malloc(n * sizeof(char));
-            if ((*matriz)[i] == NULL) {
-                printf("Erro na alocacao");
-                exit(1);
-            }
+    }
+    for(int i=0; i< n; i++){
+        matriz[i] = (char*) malloc(n * sizeof(char));
+
+        if(matriz[i] == NULL){
+            printf("Erro na alocacao");
+            exit(1);
         }
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                (*matriz)[i][j] = '\0';
-            }
+        for(int j=0; j<n;j++){
+            matriz[i][j] = 'p';
         }
     }
+    return matriz;
 }
 
-int main() {
-    char** matriz;
-    int m = 3, n = 4;
-
-    alocaMatriz(&matriz, m, n);
-
-    matriz[0][0] = 'A';
-    matriz[1][1] = 'B';
-    matriz[2][2] = 'C';
-
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            printf("%c ", matriz[i][j]);
+void imprime_matriz(char** matriz, int m, int n){
+    for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+            printf("[%c]", matriz[i][j]);
         }
         printf("\n");
     }
+}
 
-    for (int i = 0; i < m; i++) {
-        free(matriz[i]);
-    }
-    free(matriz);
-
+int main(){
+    int m = 5;
+    int n = 5;
+    char** matriz = inicia_matriz(m,n);
+    imprime_matriz(matriz,m,n);
     return 0;
 }
