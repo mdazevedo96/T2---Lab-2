8b4bd04179f3875246ec8d3695a15f8c48b0708e
HLKellermann <helena.kellermann@acad.ufsm.br>
2024-09-26 12:49:56 -0300

    Create prinMat.h

---
+++ mainV2.c/prinMat.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_n_lin(int i);
+
+void print_n_col(int c);
+
+void print_chars(char** m, int c, int i);
+
+void print_bar(int c);
+
+void print_mat(char** mat, int lin, int col);
