1b9914425f6b7afbf6bfe2516b82dfa5c5a66f29
HLKellermann <helena.kellermann@acad.ufsm.br>
2024-10-02 16:12:48 -0300

    Update main.c
    Funções passadas para funcoes.c e .h (falta a busca_palavras);

--- mainV2.c/main.c
+++ mainV2.c/main.c
@@ -3,66 +3,17 @@
 #include <string.h>
 #include <ctype.h>
 #include "printMat.h"
+#include "funcoes.h"
 
-void le_tamanho(int* l, int* c)
+/*só falta essa, separar em mais funcoes. E depois que estiver tudo rodando, adicionar em funcoes.c e funcoes.h*/
+void busca_palavras()
+/*??? Porvavelmente há um jeito melhor de fazer essa parte*/
 {
-    printf("INSIRA O TAMANHO DA MATRIZ(LINHA  COLUNA): ");
-    scanf(" %d %d", l, c);
-    while((getchar()) != '\n');
+    char palavras[800];
+    printf("DIGITE AS PALAVRAS QUE DESEJA PROCURAR NO CAÇA-PALAVRA: ");
+    fgets(palavras, sizeof(palavras), stdin);
+    printf("%s", palavras);
 }
-
-char ** inicia_matriz(int l, int c)
-{
-    char ** matriz = (char **) malloc (l * sizeof(char*));
-    if (matriz == NULL){
-        printf("ERRO DE ALOCACAO");
-        exit(1);
-    }
-    for(int i=0; i<l; i++){
-        matriz[i] = (char*) malloc(c * sizeof(char));
-
-        if(matriz[i] == NULL){
-            printf("ERRO NA ALOCACAO");
-            exit(1);
-        }
-    }
-    for(int i=0; i<l; i++){
-        for(int j=0; j<c; j++){
-            matriz[i][j] = '\0';
-        }
-    }
-    return matriz;
-}
-
-int preenche_matriz(char*** mat, int l, int c)
-{
-    int k = 0;
-    char aux[800];
-
-    printf("DIGITE UM TEXTO: ");
-    fgets(aux, sizeof(aux), stdin);
-
-    for(int i = 0; i < l; i++) {
-        for(int j = 0; j < c; j++) {
-            while(aux[k] == ' ') {k += 1;}
-            if(aux[k] == '\n' || aux[k] == '\0') {return 0;}
-            (*mat)[i][j] = toupper(aux[k]);
-            k += 1;
-        }
-    }
-    return 1;
-}
-
-void libera_matriz(char** mat, int l)
-{
-    for (int i = 0; i < l; i++) {
-        free(mat[i]);
-    }
-    free(mat);
-}
-
-/*só falta essa, separar em mais funcoes. E depois que estiver tudo rodando, criar funcoes.c e funcoes.h*/
-void busca_palavras(){}
 
 int main()
 {
@@ -75,6 +26,8 @@ int main()
     preenche_matriz(&matriz, m, n);
 
     print_mat(matriz, m, n);
+
+    busca_palavras();
 
     libera_matriz(matriz, m);
 
