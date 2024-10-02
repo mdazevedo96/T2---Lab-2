821c92af01d837e8e7d36f6eb90eeec993c3ea84
HLKellermann <helena.kellermann@acad.ufsm.br>
2024-10-01 21:50:26 -0300

    Update printMat.c
    Correção de erros e padronização

--- mainV2.c/printMat.c
+++ mainV2.c/printMat.c
@@ -4,32 +4,41 @@
 
 void print_n_lin(int i)
 {
-    printf("%d ┃", i+1);
+    printf("%2d ┃", i+1);
 }
 
 void print_n_col(int c)
 {
-    printf("\n    ");
+    printf("\n     ");
     for(int x = 0; x < c; x++)
-        printf("   %d  ", x+1);
+    {
+        if(x>=10)
+            printf("  %d  ", x+1);
+        else
+            printf("  %d   ", x+1);
+    }
 }
+
 void print_chars(char** m, int c, int i)
 {
-    for(int j = 0; j < c; j++){
+    for(int j = 0; j < c; j++)
+    {
         if(m[i][j] == '\0')
             printf("     ┃");
         else
             printf("  %c  ┃", m[i][j]);
     }
 }
+
 void print_bar(int c)
 {
-    printf("\n    ");
-        for(int x = 0; x < c; x++){
-            printf("┃━━━━━");
-            if(x == (c-1))
-                printf("┃\n  ");
-        }
+    printf("\n     ");
+    for(int x = 0; x < c; x++)
+    {
+        printf("┃━━━━━");
+        if(x == (c-1))
+            printf("┃\n  ");
+    }
 }
 
 void print_mat(char** mat, int lin, int col)
