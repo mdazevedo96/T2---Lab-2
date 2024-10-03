e94dbf9d74440e37a15ee999f662ee381c4e11f6
HLKellermann <helena.kellermann@acad.ufsm.br>
2024-10-02 21:59:10 -0300

    Update main.c
    - leitura da string 'palavras' (palavras a serem pesquisadas); 
    - 1º palavra em 'palavras' copiada para  a string (a ser) 'buscada';
    - eliminação da primeira palavra em 'palavras';

--- mainV2.c/main.c
+++ mainV2.c/main.c
@@ -6,13 +6,43 @@
 #include "funcoes.h"
 
 /*só falta essa, separar em mais funcoes. E depois que estiver tudo rodando, adicionar em funcoes.c e funcoes.h*/
-void busca_palavras()
 /*??? Porvavelmente há um jeito melhor de fazer essa parte*/
+void busca_palavras()
 {
     char palavras[800];
+    char buscada[50];
+    int tam, t, nvTam;
+
     printf("DIGITE AS PALAVRAS QUE DESEJA PROCURAR NO CAÇA-PALAVRA: ");
     fgets(palavras, sizeof(palavras), stdin);
-    printf("%s", palavras);
+
+    tam = strlen(palavras);
+    palavras[tam-1] = '\0';
+
+    printf("\nstring palavras: %s. Tam string: %d. Ultimo char(deve ser vazio): (%c)", palavras, tam, palavras[tam-1]);
+
+//separa a primeira palavra da string 'palavras' pra poder buscar a mesma;
+    for(int i = 0; palavras[i] != ' '; i++){
+        buscada[i] = palavras[i];
+        if(palavras[i+1] == ' '){
+            buscada[i+1] = '\0';
+            t = strlen(buscada)+1;
+        }
+    }
+
+    printf("\n%s. tam: %d", buscada, t);
+    nvTam = tam-t;
+
+//elimina palavra ja buscada da string 'palavras';
+    printf("\n%c", palavras[t]);
+
+    for(int i=0; i<nvTam; i++){
+        palavras[i] = palavras[t];
+        t += 1;
+    }
+    palavras[nvTam] = '\0';
+
+    printf("\nstring palavras:%s. Tam string: %d. Ultimo char(deve ser vazio): (%c)", palavras, nvTam, palavras[nvTam-1]);
 }
 
 int main()
